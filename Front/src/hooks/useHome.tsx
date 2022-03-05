acd28ce0e7e2b5cf556729bfc61dc137c728bf61
KENNEDOUANLA <dilanekenne26@gmail.com>
2022-03-05 11:04:37 +0100

    CI

--- Front/src/hooks/useHome.tsx
+++ Front/src/hooks/useHome.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { endpoint, ProductObject as Product } from "../App";
 
 const useHome = () => {
